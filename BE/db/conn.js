d5b652d453785559507034d76b4fa95a2d948331
Andres Munoz <and54@hotmail.com>
2021-12-28 16:50:58 -0500

    back-end finished

---
+++ BE/db/conn.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require("mongodb");
+const connectionString = process.env.ATLAS_URI;
+const client = new MongoClient(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+let dbConnection;
+
+module.exports = {
+  connectToServer: callback => {
+    client.connect((err, db) => {
+      if (err || !db) return callback(err);
+
+      dbConnection = db.db(process.env.DB_NAME);
+      console.log("Successfully connected to MongoDB.");
+
+      return callback();
+    });
+  },
+
+  getDb: () => dbConnection
+};
--- BE/routes/index.js
+++ BE/routes/index.js
@@ -1,9 +1,41 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const dbo = require('../db/conn');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const collection = 'urlList';
+
+router.get('/:shortUrl', async (req, res, next) => {
+ const { shortUrl } = req.params;
+  const dbConnect = dbo.getDb();
+  dbConnect.collection(collection).findOne({ shortUrl }, (err, result) => {
+    if (err) res.status(400).send('Error fetching shortUrl!');
+    else {
+      console.log('fetched data', result);
+      if (!result?.url) res.status(400).send('shortUrl doesn\'t exists!');
+      else res.json({ ...result });
+    }
+  });
 });
+
+router.post('/', async(req, res, next) => {
+  const { url } = req.body;
+  if (!url) res.status(400).send('URL not received');
+  const shortUrl = createShort();
+  const insertDoc = { url, shortUrl };
+  const dbConnect = dbo.getDb();
+  dbConnect.collection(collection).insertOne(insertDoc, (err, result) => {
+    if (err) res.status(400).send("Error inserting url!");
+    else {
+      console.log(`Added a new match with id ${result.insertedId}`);
+      res.json({ id: result.insertedId, ...insertDoc })
+    }
+  });
+});
+
+const createShort = (size = 8) => {
+  const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const random = () => Math.floor(Math.random() * letters.length);
+  return [...new Array(size)].map(() => letters.charAt(random())).join('');
+}
 
 module.exports = router;
